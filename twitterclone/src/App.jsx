f27c8ad9b0ea3dfbab73c1ee21c67db679a058cc
Ayush Gusain <ayugusain1482001@gmail.com>
2023-06-24 09:00:17 +0530

    up

--- twitterclone/src/App.jsx
+++ twitterclone/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import WebFont from 'webfontloader';
 import Menubar from './components/menu-bar'
 import Sidebar from './components/sidebar'
+import logo from "./assets/twitterlogo.png"
 import Maincontent from './components/main-content'
-import {SimpleGrid,Box,Grid,GridItem,Flex}from'@chakra-ui/react'
+import {Box,Grid,GridItem,Flex,Heading, HStack}from'@chakra-ui/react'
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 
@@ -11,14 +14,30 @@ import {SimpleGrid,Box,Grid,GridItem,Flex}from'@chakra-ui/react'
 
 
 function App() {
+  const { loginWithRedirect,logout,isAuthenticated} = useAuth0();
  return<>
-<Grid templateColumns="3fr 5fr 4fr" className='mainContainer' >
-  <GridItem minHeight="200px" className='asidebar' >
-    <Flex justifyContent="center" gap="20px"><Menubar /></Flex>
-  </GridItem>
-  <GridItem minHeight="100vh" borderLeft="1px solid gray" className='mainbox'><Maincontent /></GridItem>
-  <GridItem minHeight="100vh" borderLeft="1px solid gray" className='sidebox'><Sidebar /></GridItem>
-</Grid>
+            {isAuthenticated ? (
+              <Grid templateColumns="3fr 5fr 4fr" className='mainContainer' >
+              <GridItem minHeight="200px" className='asidebar' >
+                <Flex justifyContent="center" gap="20px"><Menubar /></Flex>
+              </GridItem>
+              <GridItem minHeight="100vh" borderLeft="1px solid gray" className='mainbox'><Maincontent /></GridItem>
+              <GridItem minHeight="100vh" borderLeft="1px solid gray" className='sidebox'><Sidebar /></GridItem>
+            </Grid>
+           ): (
+    <Box className='log' > 
+    <HStack>
+      {/* <img src={logo} alt="logo" /> */}
+      <img src="https://img.icons8.com/?size=512&id=5MQ0gPAYYx7a&format=png" alt="logo" />
+      <Heading > Twitter clone </Heading>
+    </HStack>
+    <p>just share your throughts with world...</p>
+      <button className='but' onClick={() => loginWithRedirect()}>Log In</button>
+    </Box>
+    
+    )
+            }
+
   </>
 }
 export default App
